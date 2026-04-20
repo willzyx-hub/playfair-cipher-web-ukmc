955fbcda1f7e1e0ae92815da06901575646a523e
Kevin Winardi <kevinwinardi192005@gmail.com>
2026-04-20 18:26:01 +0700

    Add missing translation

--- app/components/HeaderButton.tsx
+++ app/components/HeaderButton.tsx
@@ -29,7 +29,7 @@ export function HeaderButton({ locale, setLocale, setIsHelpModalOpen }: HeaderBu
                     <svg className="size-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5" />
                     </svg>
-                    <span>Beranda</span>
+                    <span>{t('Home')}</span>
                 </div>
             </Button>
 
